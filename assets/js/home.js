ba7042b9f2bcef77f6577bef1bd682bc1c92362c
centeno <centeno@users.noreply.github.com>
2021-03-26 13:36:19 +0000

    deploy: 4dfd9112ef3a344c842eb735214ccdd7b779bd05

---
+++ assets/js/home.js
@@ -0,0 +1,9 @@
+(function($) {
+
+  if ($('#carouselExampleVertical1').length > 0) {
+    ContinuousCarousel('carouselExampleVertical1');
+    ContinuousCarousel('carouselExampleVertical2');
+    ContinuousCarousel('carouselExampleVertical3');
+    ContinuousCarousel('carouselExampleVertical4');
+  }
+})(jQuery);
